be6fa770df645eba49fb1bb188ced9d6e9bec1ed
ggbond268 <i@ggbond.cc>
2026-09-07 15:01:41 +0800

    feat(menu-bar-hidden): restore plugin with GPL notices

--- Tests/Core/Plugins/Dynamic/PluginRuntimeActionSnapshotTests.swift
+++ Tests/Core/Plugins/Dynamic/PluginRuntimeActionSnapshotTests.swift
@@ -32,6 +32,7 @@ import LaunchControlPlugin
 import LaunchpadPlugin
 import LockScreenPlugin
 import MacSettingsPlugin
+import MenuBarHiddenPlugin
 import MicrophoneMutePlugin
 import MiddleClickPlugin
 import NightShiftPlugin
@@ -317,6 +318,7 @@ final class PluginRuntimeActionSnapshotTests: XCTestCase {
         .init(pluginID: "launchpad", makeProvider: LaunchpadPluginFactory.makeProvider),
         .init(pluginID: "lock-screen", makeProvider: LockScreenPluginFactory.makeProvider),
         .init(pluginID: "mac-settings", makeProvider: MacSettingsPluginFactory.makeProvider),
+        .init(pluginID: "menu-bar-hidden", makeProvider: MenuBarHiddenPluginFactory.makeProvider),
         .init(pluginID: "microphone-mute", makeProvider: MicrophoneMutePluginFactory.makeProvider),
         .init(pluginID: "middle-click", makeProvider: MiddleClickPluginFactory.makeProvider),
         .init(pluginID: "night-shift", makeProvider: NightShiftPluginFactory.makeProvider),
--- scripts/e2e/mactools-e2e.sh
+++ scripts/e2e/mactools-e2e.sh
@@ -1783,6 +1783,7 @@ verify_code_session() {
         -only-testing:MacToolsTests/LaunchControlCanonicalActionTests \
         -only-testing:MacToolsTests/LaunchpadPluginActionTests \
         -only-testing:MacToolsTests/LockScreenPluginTests \
+        -only-testing:MacToolsTests/MenuBarHiddenPluginTests \
         -only-testing:MacToolsTests/MicrophoneMutePluginTests \
         -only-testing:MacToolsTests/MiddleClickPluginTests \
         -only-testing:MacToolsTests/NightShiftPluginTests \
